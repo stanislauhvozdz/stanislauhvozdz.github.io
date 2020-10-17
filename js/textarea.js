1be3a0c653f4591ac0b6fa2e6627ea83d5efb4eb
stanislauhvozdz <66819873+stanislauhvozdz@users.noreply.github.com>
2020-10-17 10:59:22 +0300

    Add files via upload

--- js/textarea.js
+++ js/textarea.js
@@ -1,25 +1,5 @@
 
-let backForm = document.querySelector('.back-form');
-let backField = document.querySelector('.back-field');
-let charCounterOdin = document.querySelector('.char-counter-odin');
-let submitButton = document.querySelector('.submit-button');
 
-backField.oninput = function () {
-  charCounterOdin.textContent = backField.value.length;
-
-  if (backField.value.length > 200) {
-    backForm.classList.add('warning');
-    submitButton.disabled = true;
-  } else if (backField.value.length < 6) {
-    backForm.classList.add('warning');
-    submitButton.disabled = true;
-  } 
-  else { 
-    backForm.classList.remove('warning');
-    submitButton.disabled = false;
-    }
-
-};
 const headerNavList = document.querySelector('.header-nav--list');
 const burgerButton = document.querySelector('.burger');
 
@@ -42,3 +22,24 @@ categoriesList.addEventListener('change', () => {
     }
   });
 });
+let backForm = document.querySelector('.back-form');
+let backField = document.querySelector('.back-field');
+let charCounterOdin = document.querySelector('.char-counter-odin');
+let submitButton = document.querySelector('.submit-button');
+
+backField.oninput = function () {
+  charCounterOdin.textContent = backField.value.length;
+
+  if (backField.value.length > 200) {
+    backForm.classList.add('warning');
+    submitButton.disabled = true;
+  } else if (backField.value.length < 6) {
+    backForm.classList.add('warning');
+    submitButton.disabled = true;
+  } 
+  else { 
+    backForm.classList.remove('warning');
+    submitButton.disabled = false;
+    }
+
+};
